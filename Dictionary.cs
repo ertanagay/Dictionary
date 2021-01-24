6db1a2ccfa5aca7635b4af38dcf3ac9eb6d0f344
Ertan AGAY <ertanagay@gmail.com>
2021-01-25 01:39:08 +0300

    Proje dosyası ekle.

---
+++ Dictionary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class Dictionary<TKey,TValue>
+    {
+        TKey[] KeyArray;
+        TValue[] ValueArray;
+
+        TKey[] tempKeyArray;
+        TValue[] tempValueArray;
+
+        public Dictionary()
+        {
+            KeyArray = new TKey[0];
+            ValueArray = new TValue[0];
+
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            tempKeyArray = KeyArray;
+            tempValueArray = ValueArray;
+
+            KeyArray = new TKey[KeyArray.Length + 1];
+            ValueArray = new TValue[ValueArray.Length + 1];
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                KeyArray[i] = tempKeyArray[i];
+            }
+
+            for (int i = 0; i < tempValueArray.Length; i++)
+            {
+                ValueArray[i] = tempValueArray[i];
+            }
+
+            KeyArray[KeyArray.Length - 1] = key;
+            ValueArray[ValueArray.Length - 1] = value;
+
+
+        }
+
+        public TKey[] Items1
+        {
+            get { return KeyArray; }
+        }
+
+        public TValue[] Items2
+        {
+            get { return ValueArray; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> il_TelefonKodu = new Dictionary<string, int>();
+            il_TelefonKodu.Add("Ankara", 312);
+            il_TelefonKodu.Add("istanbul", 212);
+            il_TelefonKodu.Add("Ordu", 552);
+            il_TelefonKodu.Add("Aydın", 256);
+            il_TelefonKodu.Add("Manisa", 236);
+            il_TelefonKodu.Add("Samsun", 362);
+
+            Console.WriteLine("Listede toplam" + " " + il_TelefonKodu.Items1.Length + " " + "şehir vardır!");
+            Console.WriteLine("Bu şehir ve telefon kodları aşağıdaki gibidir;");
+
+            for (int i = 0; i < il_TelefonKodu.Items1.Length; i++)
+            {
+                Console.WriteLine(il_TelefonKodu.Items1[i] + " : "+ il_TelefonKodu.Items2[i]);
+            }
+
+
+        }
+    }
+}
